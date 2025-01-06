0e23edada6c63d94ed461326f610b0e8cbdbb9a2
Arthur Santos <arthur.saantos20@gmail.com>
2025-01-06 17:43:57 -0300

    chore: init
    O projeto como um todo será apresentado como TCC do nosso curso técnico em TI da FIEC - Fundação Indaiatubana de Educação e Cultura.
    Inicialmente, é um sistema para facilitar a denúncia e acompanhamento de problemas urbanos em nossa cidade, Indaiatuba.

---
+++ server/src/main/java/com/fiec/voz_cidada/VozCidadaApplication.java
@@ -0,0 +1,13 @@
+package com.fiec.voz_cidada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozCidadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VozCidadaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/fiec/voz_cidada/VozCidadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiec.voz_cidada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VozCidadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
