00dd8a2278c16a1913e916cb1afc3019d6637858
MagalDevs <danielgalfla12@gmail.com>
2025-02-25 21:23:24 -0300

    login feito, sem imagem

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/components/ui/label";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { AuthContext } from "@/contexts/AuthContext.tsx";
 import { useContext } from "react";
+import "./index.css"
 
 export default function LoginForm() {
     interface SignInData {
@@ -20,9 +21,9 @@ export default function LoginForm() {
 
     return (
         <div className="flex flex-col min-h-screen bg-white md:flex-row">
-            <div className="relative w-full h-40 md:h-auto md:w-1/2 bg-[#689689] rounded-b-[50%] md:rounded-none">
+            <div className="relative w-full h-40 md:h-auto md:w-1/2 bg-[--cor-primaria2] rounded-b-[50%] md:rounded-none">
                 <img
-                    src="src/pages/login/login-bg.png"
+                    src="src/pages/login/foda-removebg-preview.png"
                     alt="Login visual"
                     className="w-full h-full object-cover object-[center_20%] md:object-center rounded-b-[50%] md:rounded-none"
                 />
@@ -31,14 +32,14 @@ export default function LoginForm() {
             <div className="flex items-center justify-center w-full p-8 md:w-1/2">
                 <div className="w-full max-w-md space-y-8">
                     <div className="text-center">
-                        <h2 className="text-3xl font-bold text-[--cor-primaria] font-montserrat">Bem-vindo!</h2>
-                        <p className="mt-2 text-sm text-gray-600 font-lato">Por favor, faça login na sua conta</p>
+                        <h2 className="text-4xl font-bold text-[--cor-primaria2] font-montserrat" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>Bem-vindo!</h2>
+                        <p className="mt-2 text-lg text-gray-600 font-lato text-[--cor-primaria2]">Realize o Login pra continuar!</p>
                     </div>
 
                     <form onSubmit={handleSubmit(handleSignIn)} className="mt-8 space-y-6 font-lato">
                         <div className="space-y-4">
                             <div>
-                                <Label htmlFor="login" className="text-[--cor-primaria]">
+                                <Label htmlFor="login" className='font-lato text-md'>
                                     Login
                                 </Label>
                                 <Input
@@ -47,12 +48,12 @@ export default function LoginForm() {
                                     type="text"
                                     autoComplete="username"
                                     required
-                                    className="mt-1 border-[--cor-primaria]"
+                                    className="mt-1 border-black font-lato"
                                     placeholder="seu@email.com"
                                 />
                             </div>
                             <div>
-                                <Label htmlFor="password" className="text-[--cor-primaria]">
+                                <Label htmlFor="password" className='font-lato text-md'>
                                     Senha
                                 </Label>
                                 <Input
@@ -61,26 +62,36 @@ export default function LoginForm() {
                                     type="password"
                                     autoComplete="current-password"
                                     required
-                                    className="mt-1 border-[--cor-primaria]"
+                                    className="mt-1 border-black font-lato"
                                     placeholder="••••••••"
                                 />
                             </div>
                         </div>
                         <div>
-                            <Button type="submit" className="w-full bg-[--cor-primaria] hover:bg-[--cor-primaria2] text-white">
+                            <Button type="submit" className="w-full bg-[--cor-primaria2] hover:bg-[--cor-primaria] text-white hover:duration-150 text-lg md:text-sm">
                                 Entrar
                             </Button>
                         </div>
                     </form>
 
-                    <p className="mt-2 text-sm text-center text-gray-600 font-lato">
-                        Não tem uma conta?{" "}
-                        <a href="#" className="font-medium text-[--cor-primaria] hover:text-[--cor-primaria2]">
-                            Registre-se
-                        </a>
-                    </p>
+
+
+                    <div className='flex justify-between'>
+                        <p className='mt-2 text-sm text-center text-gray-600 font-lato hover:underline hover:text-black'>
+                            <a href="#">
+                                Esqueceu a senha?
+                            </a>
+                        </p>
+
+                        <p className="mt-2 text-sm text-center text-gray-600 font-lato">
+                            Não tem conta?{" "}
+                            <a href="#" className="font-medium text-[--cor-primaria2] hover:text-[--cor-primaria] hover:underline">
+                                Registre-se
+                            </a>
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </div>  
         </div>
     );
 }
