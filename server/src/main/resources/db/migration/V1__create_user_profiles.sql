16c9c2e94b7efff10dfcdda8cb0708011cc53f35
Arthur Santos <arthur.santos@vilt.local>
2025-06-13 11:48:09 -0300

    chore: wip

--- server/src/main/resources/db/migration/V1__create_user_profiles.sql
+++ server/src/main/resources/db/migration/V1__create_user_profiles.sql
@@ -1,15 +1,15 @@
 CREATE TABLE auth_user (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     fcm_token TEXT,
-    login TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT NOT NULL,
-    auth_status TEXT NOT NULL
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    auth_status VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE usuario (
-    id BIGSERIAL PRIMARY KEY,
-    auth_user_id INT NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    auth_user_id BIGINT NOT NULL,
     nome VARCHAR(255) NOT NULL,
     data_nascimento DATE NOT NULL,
     cpf VARCHAR(11) NOT NULL,
@@ -18,20 +18,21 @@ CREATE TABLE usuario (
     bairro VARCHAR(255),
     cidade VARCHAR(255),
     uf CHAR(2),
-    data_cadastro TIMESTAMP NOT NULL,
+    data_cadastro DATETIME NOT NULL,
     CONSTRAINT fk_auth_user
         FOREIGN KEY (auth_user_id)
-        REFERENCES auth_user(id)
-        ON DELETE CASCADE
+            REFERENCES auth_user(id)
+            ON DELETE CASCADE
 );
 
+-- Tabela funcionario_prefeitura
 CREATE TABLE funcionario_prefeitura (
-    id SERIAL PRIMARY KEY,
-    auth_user_id INT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY, -- SERIAL no Postgres é INT AUTO_INCREMENT no MySQL
+    auth_user_id BIGINT NOT NULL,
     cpf VARCHAR(11) NOT NULL,
     cargo VARCHAR(255) NOT NULL,
     secretaria VARCHAR(255) NOT NULL,
-    data_cadastro TIMESTAMP NOT NULL,
+    data_cadastro DATETIME NOT NULL,
     CONSTRAINT fk_auth_user
         FOREIGN KEY (auth_user_id)
             REFERENCES auth_user(id)
